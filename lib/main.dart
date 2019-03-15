5e81074242c31cd71e6a3d99f887a3f03c06c795
jackywang <jackyloop@outlook.com>
2019-03-15 16:05:29 +0800

    test commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutteruichallenges/bookstore/bookStore.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
